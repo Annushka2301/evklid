0d9973d01ca9d7564af784f20e89b33b244301d9
Solokhina Anna <morozovaannuska@mail.ru>
2022-02-20 23:02:45 +0300

    first commit

---
+++ accordion.js
@@ -0,0 +1,17 @@
+$( function() {
+  $( "#accordion" ).accordion();
+} );
+
+$( "#accordion" ).accordion({
+  heightStyle: "content"
+});
+
+$(function() {
+  $("#accordion").accordion({
+      collapsible: true,
+      active: false,
+  });
+});
+
+
+
---
+++ burger.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#burger').addEventListener('click', function() {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#burger').addEventListener('click', function() {
+    document.querySelector('#burger').classList.toggle('active')
+  })
+})
